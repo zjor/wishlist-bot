d0c3a877781a942c7d89d98a7da3573cff9be9be
Sergey Royz <zjor.se@gmail.com>
2023-04-28 14:26:13 +0200

    added db params to deployment

--- ops/scripts/create-secrets.sh
+++ ops/scripts/create-secrets.sh
@@ -9,6 +9,9 @@ APP=wishlist-bot
 kubectl delete secret ${APP}-secrets -n ${NAMESPACE}
 
 kubectl create secret generic ${APP}-secrets \
+  --from-literal=SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL} \
+  --from-literal=SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME} \
+  --from-literal=SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD} \
   --from-literal=TELEGRAM_BOTUSERNAME=${TELEGRAM_BOTUSERNAME} \
   --from-literal=TELEGRAM_BOTTOKEN=${TELEGRAM_BOTTOKEN} \
   --from-literal=TELEGRAM_CREATORID=${TELEGRAM_CREATORID} \
