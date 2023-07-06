9490ad2e1073f299df419d7477147c24a6121d59
Sergey Royz <zjor.se@gmail.com>
2023-07-06 15:58:47 +0200

    stats implemented

--- src/main/java/com/github/zjor/controller/UserController.java
+++ src/main/java/com/github/zjor/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.github.zjor.controller;
 
+import com.github.zjor.controller.dto.JProfileStatsResponse;
 import com.github.zjor.controller.dto.JUser;
+import com.github.zjor.domain.ItemStatus;
 import com.github.zjor.domain.User;
+import com.github.zjor.domain.WishlistItem;
 import com.github.zjor.ext.spring.auth.AuthUser;
+import com.github.zjor.repository.WishlistItemRepository;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +20,38 @@ import static com.github.zjor.controller.XHttpHeaders.X_TELEGRAM_USER;
 @RequestMapping("api/user")
 public class UserController {
 
+    private final WishlistItemRepository wishlistItemRepository;
+
+    public UserController(WishlistItemRepository wishlistItemRepository) {
+        this.wishlistItemRepository = wishlistItemRepository;
+    }
+
     @GetMapping("me")
     public JUser me(@AuthUser User user) {
         return JUser.Converter.build(user);
+    }
+
+    @GetMapping("me/stats")
+    public JProfileStatsResponse getStats(@AuthUser User user) {
+        int all = 0;
+        int _public = 0;
+        int done = 0;
+
+        for (WishlistItem item: wishlistItemRepository.findByOwner(user)) {
+            if (item.getStatus() != ItemStatus.ARCHIVED) {
+                all++;
+
+                if (item.isPublic() && item.getStatus() != ItemStatus.DONE) {
+                    _public++;
+                }
+
+                if (item.getStatus() == ItemStatus.DONE) {
+                    done++;
+                }
+            }
+        }
+
+        return new JProfileStatsResponse(all, _public, done);
     }
 
 }
---
+++ src/main/java/com/github/zjor/controller/dto/JProfileStatsResponse.java
@@ -0,0 +1,4 @@
+package com.github.zjor.controller.dto;
+
+public record JProfileStatsResponse(int allItemsCount, int publicCount, int doneCount) {
+}
