e996fcf91f8d3cdedde7b6fa4b8c3d52272f3e02
Sergey Royz <zjor.se@gmail.com>
2023-08-31 16:08:21 +0200

    site loader tool

---
+++ tools/parser/app.py
@@ -0,0 +1,25 @@
+import logging
+from selenium import webdriver
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def store_page_source(url: str, out_filename: str, headless: bool = True):
+    options = webdriver.FirefoxOptions()
+    if headless:
+        options.add_argument('--headless')
+
+    browser = webdriver.Firefox(options=options)
+
+    browser.get(url)
+    with open(out_filename, "w") as f:
+        f.write(browser.page_source)
+    browser.close()
+
+
+if __name__ == "__main__":
+    store_page_source(
+        url="https://www.amazon.com/dp/B0C947C9QS/",
+        out_filename="out.html"
+    )
